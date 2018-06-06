8069af48928ec435d881f083989c3e14c79b1218
John O'Connor <oconnojb@gmail.com>
2018-06-06 15:34:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase()
+}
+
+var bestCustomer
+
+function setBestCustomer() {
+  console.log(bestCustomer)
+  bestCustomer = "not bob"
+  console.log(bestCustomer)
+  console.log(window.bestCustomer)
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob"
+}
+
+const leastFavoriteCustomer = "john"
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "anyone else"
+}
