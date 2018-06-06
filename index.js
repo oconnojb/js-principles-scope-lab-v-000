9b6de7464ac3b3ad68bbba13ecd0e7453285d4e7
John O'Connor <oconnojb@gmail.com>
2018-06-06 15:28:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,5 @@ var customerName = "bob"
 
 
 function upperCaseCustomerName() {
-  console.log("before:", window.customerName)
   customerName = customerName.toUpperCase()
-  console.log("after:", window.customerName)
-  return customerName
 }
